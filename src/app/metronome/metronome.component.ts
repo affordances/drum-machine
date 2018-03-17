725dc075fce3bd3c6f808c947e5dacd19e4d623f
Gabriel Duquette <gabrielduquette@gmail.com>
2018-03-17 19:06:03 -0400

    add hats and snare

--- src/app/metronome/metronome.component.ts
+++ src/app/metronome/metronome.component.ts
@@ -9,30 +9,46 @@ import { Howl, Howler } from "howler";
 export class MetronomeComponent {
   bpm: number = 130;
   playing: boolean = false;
-  activeBeats = [true, false, false, false];
   timer: number;
   beat: number = 0;
-  sound = new Howl({src: ['./assets/click1.mp3']});
+  hats = [false, false, false, false];
+  hat = new Howl({src: ['./assets/chh.wav']});
+  snares = [false, false, false, false];
+  snare = new Howl({src: ['./assets/sd.wav']});
+  kicks = [true, false, false, false];
+  kick = new Howl({src: ['./assets/kick.wav']});
 
   constructor() {}
 
   ngOnInit() {
   }
 
-  toggleBeat(i) {
-    this.activeBeats[i] = !this.activeBeats[i];
+  toggleBeat(instrument, beatLocation) {
+    if (instrument === 'hats') {
+      this.hats[beatLocation] = !this.hats[beatLocation];
+    } else if (instrument === 'snares') {
+      this.snares[beatLocation] = !this.snares[beatLocation];
+    } else {
+      this.kicks[beatLocation] = !this.kicks[beatLocation];
+    }
   }
 
   updateBpm(): void {
     if (this.playing) {
       clearInterval(this.timer);
-      this.timer = setInterval(() => this.playClick(), (15 / this.bpm) * 1000);
+      this.timer = setInterval(() => this.playSounds(), (15 / this.bpm) * 1000);
     }
   }
 
-  playClick() {
-    if (this.activeBeats[this.beat]) {
-      this.sound.play();
+  playSounds() {
+    if (this.hats[this.beat]) {
+      this.hat.play();
+    }
+    if (this.snares[this.beat]) {
+      this.snare.play();
+    }
+    if (this.kicks[this.beat]) {
+      this.kick.play();
     }
     this.beat = (this.beat + 1) % 4;
   }
@@ -42,9 +58,9 @@ export class MetronomeComponent {
       clearInterval(this.timer);
       this.playing = false;
     } else {
-      this.timer = setInterval(() => this.playClick(), (15 / this.bpm) * 1000);
+      this.timer = setInterval(() => this.playSounds(), (15 / this.bpm) * 1000);
       this.playing = true;
     }
-    this.playClick();
+    this.playSounds();
   }
 }
