e3e117028915c75e9d49cc227ee41794b07699d5
0x70b1a5 <0x70b1a5@users.noreply.github.com>
2018-03-20 00:07:17 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, Output, Input } from '@angular/core';
-import { Howl } from "howler";
+import { Component } from '@angular/core';
+import { Howl } from 'howler';
 import { EventsService } from './events.service';
 
 @Component({
@@ -8,27 +8,27 @@ import { EventsService } from './events.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  bpm: number = 130;
-  playing: boolean = false;
-  timer: number;
-  beat: number = 0;
+  bpm = 130;
+  playing = false;
+  timer;
+  beat = 0;
   instruments = [];
   beatLocations = {};
   sounds = {
-    "open hat": new Howl({src: ['./assets/ohh.wav']}),
-    "closed hat": new Howl({src: ['./assets/chh.wav']}),
-    "snare": new Howl({src: ['./assets/sd.wav']}),
-    "kick": new Howl({src: ['./assets/kick.wav']})
+    'open hat': new Howl({src: ['./assets/ohh.wav']}),
+    'closed hat': new Howl({src: ['./assets/chh.wav']}),
+    'snare': new Howl({src: ['./assets/sd.wav']}),
+    'kick': new Howl({src: ['./assets/kick.wav']})
   };
 
   constructor(private es: EventsService) {
     Object.keys(this.sounds).forEach(instrument =>
       this.beatLocations[instrument] = Array(16).fill(false));
 
-    this.beatLocations["kick"][0] = true;
-    this.beatLocations["kick"][4] = true;
-    this.beatLocations["kick"][8] = true;
-    this.beatLocations["kick"][12] = true;
+    this.beatLocations['kick'][0] = true;
+    this.beatLocations['kick'][4] = true;
+    this.beatLocations['kick'][8] = true;
+    this.beatLocations['kick'][12] = true;
 
     this.instruments = Object.keys(this.sounds);
   }
@@ -43,10 +43,12 @@ export class AppComponent {
   togglePlay(event): void {
     if (!event || event.keyCode === 32) {
       if (this.playing) {
+        alert('stopping')
         clearInterval(this.timer);
         this.playing = false;
         this.beat = 0;
       } else {
+        alert('playing')
         this.timer = setInterval(() => this.es.playSoundsEvent.emit(), (15 / this.bpm) * 1000);
         this.playing = true;
         this.beat = 0;
@@ -64,7 +66,7 @@ export class AppComponent {
     const instruments = Object.keys(this.sounds);
     instruments.forEach(instrument => {
       if (this.beatLocations[instrument][this.beat]) {
-        this.sounds[instrument].play()
+        this.sounds[instrument].play();
       }
     });
     this.beat = (this.beat + 1) % 16;
